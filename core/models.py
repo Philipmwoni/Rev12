8a558b1f2ccef059e128b57cdf76a25c4d00b43b
philipsmwoni <philipsmwoni86@gmail.com>
2026-01-08 23:59:47 +0300

    commit

--- core/models.py
+++ core/models.py
@@ -7,7 +7,6 @@ from django.db import models
 
 # Create your models here.
 
-
 class Expense(models.Model):
     title = models.CharField(max_length=100)
     amount = models.FloatField()
@@ -15,3 +14,16 @@ class Expense(models.Model):
 
     def __str__(self):
         return f"{self.title} - {self.amount} on {self.date}"
+    
+
+
+
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100, unique=True)
+    joined_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
